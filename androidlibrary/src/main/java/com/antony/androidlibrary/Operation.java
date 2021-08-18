d347cc8e595d41cf7c21289e3c6ae2580c39f27b
Antonyraja-f <f.antonyraja@gmail.com>
2021-08-18 15:23:56 +0530

    Check

---
+++ androidlibrary/src/main/java/com/antony/androidlibrary/Operation.java
@@ -0,0 +1,12 @@
+package com.antony.androidlibrary;
+
+public class Operation {
+
+    public static int plus(int a, int b){
+        return a+b;
+    }
+
+    public static int minus(int a, int b){
+        return a-b;
+    }
+}
